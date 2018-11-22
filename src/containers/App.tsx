37199f344e90bc7198cf83641e231d689678baec
Antonios Karagiannis <antokarag@gmail.com>
2018-11-22 01:00:18 -0500

    tslint fixes

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+/**
+ * Application Container
+ * the main container of the whole application
+ */
+import * as React from 'react';
+// @todo make this alias to be empty in production?
 import { hot } from 'react-hot-loader';
 
-const App = () => <div>Hello World!</div>;
+const app: React.FunctionComponent = (): React.ReactElement<
+  React.ReactNode
+> => <div>Hello World!</div>;
 
-export default hot(module)(App);
+const containerApp: React.FunctionComponent = hot(module)(app);
+
+export { containerApp as App };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import App from 'Containers/App';
+/**
+ * the main entry point of the app
+ */
+import { App } from 'Containers/App';
+import * as React from 'react';
 import { render } from 'react-dom';
 
-render(
-  <App/>,
-  document.getElementById('root'),
-);
+render(<App />, document.getElementById('root'));
