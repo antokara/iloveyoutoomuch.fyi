6918fc50e9c57df2a998fa6a6ef877e23788097a
Antonios Karagiannis <antokarag@gmail.com>
2018-12-13 22:39:49 -0500

    add loading spinner

--- src/components/saveTheDate/Loading.tsx
+++ src/components/saveTheDate/Loading.tsx
@@ -1,16 +1,27 @@
 /**
  * Save The Date Loading Component
  */
+import { CircularProgress } from '@material-ui/core';
 import * as React from 'react';
-import { graphql } from 'react-apollo';
 import styledComponents from 'styled-components';
 
 const Wrapper: React.FunctionComponent = styledComponents.div`
-  color: blue;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Loading: React.FunctionComponent = (): React.ReactElement<
   React.ReactNode
-> => <Wrapper>Loading...</Wrapper>;
+> => (
+  <Wrapper>
+    <CircularProgress />
+  </Wrapper>
+);
 
 export { Loading };
