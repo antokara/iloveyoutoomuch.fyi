7c5ef78ceb8ca5f172147a7ea32158ce1503a395
Antonios Karagiannis <antokarag@gmail.com>
2018-12-17 23:51:13 -0500

    add og tags

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -45,6 +45,40 @@ module.exports = env =>
             name: 'viewport',
             content:
               'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+          },
+          {
+            property: 'og:title',
+            content: 'Stephanie & Antonios Wedding'
+          },
+          {
+            property: 'og:description',
+            content: 'Save the date, RSVP & Information'
+          },
+          {
+            property: 'og:type',
+            content: 'website'
+          },
+          {
+            property: 'og:locale',
+            content: 'en_US'
+          },
+          {
+            property: 'og:image',
+            content:
+              'http://images.takeshape.io/53e75ca3-0cfa-43ca-810f-2a6e6c39ecb3/dev/192fda5f-a96f-46f0-8bae-cff8851507a2/DSC00558.jpg?fit=crop&w=1080&h=1080&fp-x=0&fp-y=1'
+          },
+          {
+            property: 'og:image:secure_url',
+            content:
+              'https://images.takeshape.io/53e75ca3-0cfa-43ca-810f-2a6e6c39ecb3/dev/192fda5f-a96f-46f0-8bae-cff8851507a2/DSC00558.jpg?fit=crop&w=1080&h=1080&fp-x=0&fp-y=1'
+          },
+          {
+            property: 'og:image:width',
+            content: '1080'
+          },
+          {
+            property: 'og:image:height',
+            content: '1080'
           }
         ]
       }),
