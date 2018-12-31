19a2d2a27dee52278703d96676c74dbb595b15be
Antonios Karagiannis <antokarag@gmail.com>
2018-12-31 16:44:21 -0500

    style eventDate properly

--- src/components/layouts/themed/EventDate.tsx
+++ src/components/layouts/themed/EventDate.tsx
@@ -3,11 +3,13 @@ import styledComponents from 'styled-components';
 
 const EventDate: React.FunctionComponent = styledComponents.div`
   color: ${PALETTE.DARK_RED};
-  font-family: Rochester, sans-serif;
-  font-size: 4em;
-  text-shadow: 1px 1px 2px #000;
-  text-align: center;
-  margin-top: 1vh;
+  font-family: Caladea, sans-serif;
+  font-size: 1.5em;
+  text-shadow: 1px 1px 20px #fff, 1px 1px 10px #fff, 1px 1px 5px #fff, 1px 1px 2px #fff;
+  text-align: right;
+  margin: 0.3em auto;
+  width: 18em;
+  text-transform: uppercase;
 `;
 
 export { EventDate };
