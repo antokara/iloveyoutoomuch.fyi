cd16e833d955ff19da2046f53367c93b9848ae71
Antonios Karagiannis <antokarag@gmail.com>
2019-01-03 23:05:36 -0500

    make the header link to home

--- src/components/layouts/themed/Header.tsx
+++ src/components/layouts/themed/Header.tsx
@@ -1,13 +1,14 @@
 import { PALETTE } from 'Constants/PALETTE';
+import { ROUTES } from 'Constants/ROUTES';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
 import styledComponents from 'styled-components';
 
-const Header: React.FunctionComponent = styledComponents.div`
-  color: ${PALETTE.PINK};
+const Wrapper: React.FunctionComponent = styledComponents.div`
   font-family: Rochester, sans-serif;
-  font-size: 2em;
-  text-shadow: 1px 1px 2px #000;
   text-align: center;
   margin-top: 2vh;
+  font-size: 2em;
 
   @media (min-width: 1024px) {
     margin-top: 4vh;
@@ -19,5 +20,19 @@ const Header: React.FunctionComponent = styledComponents.div`
     font-size: 4em;
   }
 `;
+
+const Text: React.FunctionComponent = styledComponents(Link)`
+  color: ${PALETTE.PINK};
+  text-shadow: 1px 1px 2px #000;
+  text-decoration: none;
+`;
+
+const Header: React.FunctionComponent = ({
+  children
+}): React.ReactElement<React.ReactNode> => (
+  <Wrapper>
+    <Text to={ROUTES.HOME.PATH}>{children}</Text>
+  </Wrapper>
+);
 
 export { Header };
