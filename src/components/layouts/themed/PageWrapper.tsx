c98373ee851b22af20a5c088f3f4a67eb4f49568
Antonios Karagiannis <antokarag@gmail.com>
2019-01-03 21:20:12 -0500

    style gallery for desktop

--- src/components/layouts/themed/PageWrapper.tsx
+++ src/components/layouts/themed/PageWrapper.tsx
@@ -4,6 +4,10 @@ const PageWrapper: React.FunctionComponent = styledComponents.div`
   max-width: 30em;
   margin: 2em auto;
   color: white;
+
+  @media (orientation: landscape) and (min-width: 1024px) {
+    max-width: 60em;
+  }
 `;
 
 export { PageWrapper };
--- src/components/pages/gallery/Gallery.tsx
+++ src/components/pages/gallery/Gallery.tsx
@@ -5,12 +5,13 @@ import { GridList, GridListTile } from '@material-ui/core';
 import { PageWrapper } from 'Components/layouts/themed/PageWrapper';
 import { imgUrl } from 'Helpers/imgUrl';
 import * as React from 'react';
+import { default as MediaQuery } from 'react-responsive';
 
-const GridTiles: React.FunctionComponent = photos =>
+const GridTiles: React.FunctionComponent = (photos, widePhotos) =>
   photos.map((photo, index: number) => (
     <GridListTile
       key={photo.photo._id}
-      cols={[0, 3, 7, 10, 14, 17, 18].indexOf(index) !== -1 ? 2 : 1}
+      cols={widePhotos.indexOf(index) !== -1 ? 2 : 1}
     >
       <img
         src={imgUrl(photo.photo.path, '?fit=max&w=500')}
@@ -23,9 +24,16 @@ const Gallery: React.FunctionComponent = ({
   data
 }): React.ReactElement<React.ReactNode> => (
   <PageWrapper>
-    <GridList cellHeight={160} cols={3}>
-      {GridTiles(data.photos)}
-    </GridList>
+    <MediaQuery maxWidth={1224}>
+      <GridList cellHeight={160} cols={3}>
+        {GridTiles(data.photos, [0, 3, 7, 10, 14, 17, 18])}
+      </GridList>
+    </MediaQuery>
+    <MediaQuery minWidth={1225}>
+      <GridList cellHeight={230} cols={5}>
+        {GridTiles(data.photos, [0, 2, 4, 5, 6, 7, 10, 11, 14, 17, 18, 19])}
+      </GridList>
+    </MediaQuery>
   </PageWrapper>
 );
 
