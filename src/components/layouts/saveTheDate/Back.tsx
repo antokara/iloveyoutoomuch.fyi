0dbab6db4b4b024d743b99f153a00b1939a23352
Antonios Karagiannis <antokarag@gmail.com>
2019-01-10 23:54:05 -0500

    wrap back button

--- src/components/layouts/saveTheDate/Back.tsx
+++ src/components/layouts/saveTheDate/Back.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import styledComponents from 'styled-components';
 
-const Button = styledComponents(Fab)`
+const ButtonWrapper = styledComponents.div`
   position: absolute;
   right: 1rem;
   top: 1rem;
@@ -14,9 +14,11 @@ const Button = styledComponents(Fab)`
 const Back: React.FunctionComponent = (): React.ReactElement<
   React.ReactNode
 > => (
-  <Button size="small" color="primary" component={Link} to={ROUTES.HOME.PATH}>
-    <ArrowBackIosRounded />
-  </Button>
+  <ButtonWrapper>
+    <Fab size="small" color="primary" component={Link} to={ROUTES.HOME.PATH}>
+      <ArrowBackIosRounded />
+    </Fab>
+  </ButtonWrapper>
 );
 
 export { Back };
