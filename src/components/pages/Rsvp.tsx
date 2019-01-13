503f7a931920591dfbc26a8814cd67f2cab7cd4f
Antonios Karagiannis <antokarag@gmail.com>
2019-01-12 21:00:50 -0500

    add redux-form in rsvp

--- src/components/pages/Rsvp.tsx
+++ src/components/pages/Rsvp.tsx
@@ -1,10 +1,29 @@
 /**
  * Rsvp page Component
  */
+import { TextField } from '@material-ui/core';
 import { PageWrapper } from 'Components/layouts/themed/PageWrapper';
 import { MarkdownWrapper } from 'Components/shared/MarkdownWrapper';
+import { validators } from 'Helpers/validators';
 import * as React from 'react';
 import * as ReactMarkdown from 'react-markdown';
+import { Field } from 'redux-form';
+
+const renderTextField = ({
+  label,
+  input,
+  meta: { touched, invalid, error },
+  ...custom
+}) => (
+  <TextField
+    label={label}
+    placeholder={label}
+    error={touched && invalid}
+    helperText={touched && error}
+    {...input}
+    {...custom}
+  />
+);
 
 const Rsvp: React.FunctionComponent = ({
   body
@@ -12,6 +31,16 @@ const Rsvp: React.FunctionComponent = ({
   <PageWrapper>
     <MarkdownWrapper>
       <ReactMarkdown source={body} />
+      <form onSubmit={() => {}}>
+        <Field
+          component={renderTextField}
+          name="firstName"
+          label="First Name *"
+          type="text"
+          disabled={false}
+          validate={[validators.required]}
+        />
+      </form>
     </MarkdownWrapper>
   </PageWrapper>
 );
--- src/containers/pages/Rsvp.tsx
+++ src/containers/pages/Rsvp.tsx
@@ -8,12 +8,18 @@ import { Loading } from 'Components/shared/Loading';
 import * as getRsvp from 'Gql/getRsvp';
 import * as React from 'react';
 import { graphql } from 'react-apollo';
+import { reduxForm } from 'redux-form';
 
 const RsvpContainer: React.FunctionComponent = ({
   data
 }): React.ReactElement<React.ReactNode> =>
   data.loading ? <Loading /> : <RsvpComponent body={data.rsvp.body} />;
 
-const Rsvp: React.ComponentClass = graphql(getRsvp)(RsvpContainer);
+const Rsvp: React.ComponentClass = graphql(getRsvp)(
+  reduxForm({
+    form: 'coach/memberDetail/highlight',
+    enableReinitialize: true
+  })(RsvpContainer)
+);
 
 export { Rsvp };
