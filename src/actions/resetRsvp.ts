006471ff5b3a42c5e3a2f158a8d49b5aa3897b78
Antonios Karagiannis <antokarag@gmail.com>
2019-02-16 11:51:34 -0500

    reset form fields on resetRsvp action

--- src/actions/resetRsvp.ts
+++ src/actions/resetRsvp.ts
@@ -1,7 +1,9 @@
 import { ACTION_TYPES } from 'Constants/ACTION_TYPES';
 import { STATUSES } from 'Constants/STATUSES';
+import { reset } from 'redux-form';
 
 export const resetRsvp = () => dispatch => {
+  dispatch(reset('rsvp'));
   dispatch({
     type: ACTION_TYPES.RSVP,
     meta: {
